0391adcc4c67ebdee4745fc185e573df443eef9f
roniadmon <roniadmon@gmail.com>
2017-07-28 02:17:31 +0300

    first

--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -1,28 +1,104 @@
-<template>
-  <!--<v-flex md12 lg4 lg-offset8>-->
-    <!--<v-time-picker v-model="e7" format="24hr"></v-time-picker>-->
-    <!--<p>{{e7}}</p>-->
-  <!--</v-flex>-->
-  <v-layout row>
-    <v-flex xs7>
-      <div>
-        <div class="headline">Supermodel</div>
-        <div>Foster the People</div>
-      </div>
-    </v-flex>
-    <v-flex xs5>
-      <v-card-media
-        src="https://randomuser.me/api/portraits/men/85.jpg"
-        height="125px"
-        contain
-      ></v-card-media>
-    </v-flex>
-  </v-layout>
-
+<template dir="rtl">
+  <v-app style="max-width: 400px; margin: auto;" standalone>
+    <v-toolbar >
+      <v-layout row wrap>
+        <v-flex >
+          <v-menu
+            lazy
+            :close-on-content-click="false"
+            v-model="menu"
+            transition="scale-transition"
+            offset-y
+            full-width
+            :nudge-left="40"
+            max-width="400px"
+          >          <v-text-field
+            slot="activator"
+            :label="this.chosenDate" dir="rtl"
+            v-model="e3"
+            prepend-icon="event"
+            readonly
+          ></v-text-field>
+            <v-date-picker v-model="chosenDate" scrollable actions >
+              <template scope="{ save, cancel }">
+                <v-card-actions>
+                  <v-btn flat primary @click.native="cancel()">Cancel</v-btn>
+                  <v-btn flat primary @click.native="save()">Save</v-btn>
+                </v-card-actions>
+              </template>
+            </v-date-picker>
+          </v-menu>
+        </v-flex>
+      </v-layout>
+    </v-toolbar>
+    <main>
+      <v-container
+        fluid
+        style="min-height: 0;"
+        grid-list-lg
+      >
+        <v-layout row wrap>
+          <v-flex xs12 v-for="item in getEventsByDay(chosenDate)">
+            <v-card class="cyan darken-2 white--text">
+              <v-container fluid grid-list-lg>
+                <v-layout row>
+                  <v-flex xs7>
+                    <div>
+                      <div class="headline">{{item.text}}</div>
+                    </div>
+                  </v-flex>
+                  <v-flex xs5>
+                    <v-card-media
+                      :src="item.image_url"
+                      height="125px"
+                      contain
+                    ></v-card-media>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card>
+          </v-flex>
+          <v-flex xs12>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </main>
+  </v-app>
 </template>
+
+<!--<template>-->
+  <!--&lt;!&ndash;<v-flex md12 lg4 lg-offset8>&ndash;&gt;-->
+    <!--&lt;!&ndash;<v-time-picker v-model="e7" format="24hr"></v-time-picker>&ndash;&gt;-->
+    <!--&lt;!&ndash;<p>{{e7}}</p>&ndash;&gt;-->
+  <!--&lt;!&ndash;</v-flex>&ndash;&gt;-->
+  <!--<v-layout row>-->
+    <!--<v-flex xs7>-->
+      <!--<div>-->
+        <!--<div class="headline">Supermodel</div>-->
+        <!--<div>Foster the People</div>-->
+      <!--</div>-->
+    <!--</v-flex>-->
+    <!--<v-flex xs5>-->
+      <!--<v-card-media-->
+        <!--src="https://randomuser.me/api/portraits/men/85.jpg"-->
+        <!--height="125px"-->
+        <!--contain-->
+      <!--&gt;</v-card-media>-->
+    <!--</v-flex>-->
+  <!--</v-layout>-->
+
+<!--</template>-->
 
 <script>
   export default{
+    methods:{
+      getEventsByDay(date)
+      {/*if (this.schedule.date===date)*/
+        return this.schedule.events;
+      },
+        setScheduleByDate(){this.schedule=getEventsByDay(this.chosenDate);}
+    },
+    watch:{'chosenDate':'setScheduleByDate'},
     data(){
       return {
         e7: null,
@@ -32,13 +108,21 @@
             {
               "url": "http://127.0.0.1:8000/events/1/",
               "order": 1,
-              "image_url": "http://weknowyourdreams.com/image.php?pic=/images/breakfast/breakfast-08.jpg",
+              "image_url": "https://randomuser.me/api/portraits/men/86.jpg",
               "text": "ארוחת בוקר",
               "deleted": false,
               "schedule": "http://127.0.0.1:8000/schedule/1/"
+            },{
+              "url": "http://127.0.0.1:8000/events/2/",
+              "order": 2,
+              "image_url": "https://randomuser.me/api/portraits/men/87.jpg",
+              "text": "ארוחת צהריים",
+              "deleted": false,
+              "schedule": "http://127.0.0.1:8000/schedule/2/"
             }
           ]
-        }
+        },
+        chosenDate:new Date().toISOString().slice(0,10),
       };
     }
   }
