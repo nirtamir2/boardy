678e60f31ccee5fba1b56b0a4c602f4ea179fc58
Nir Tamir <nirtamir2@gmail.com>
2017-07-27 18:20:24 +0300

    add toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,47 @@
 <template>
   <div id="app">
-
+    <v-app id="example-2" standalone>
+      <v-navigation-drawer absolute persistent light :mini-variant.sync="mini" v-model="drawer" overflow>
+        <v-toolbar flat class="transparent">
+          <v-list class="pa-0">
+            <v-list-tile avatar tag="div">
+              <v-list-tile-avatar>
+                <img src="https://randomuser.me/api/portraits/men/85.jpg"/>
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>User NAme</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-btn icon @click.native.stop="mini = !mini">
+                  <v-icon>chevron_left</v-icon>
+                </v-btn>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list>
+        </v-toolbar>
+        <v-list class="pt-0" dense>
+          <v-divider></v-divider>
+          <v-list-tile v-for="item in items" :key="item.title">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar fixed class="light-blue darken-4" dark>
+        <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Boardy</v-toolbar-title>
+      </v-toolbar>
+      <main>
+        <v-container fluid>
+          <div class="title"></div>
+          <router-view></router-view>
+        </v-container>
+      </main>
+    </v-app>
   </div>
 </template>
 
@@ -10,7 +51,13 @@
     data()
     {
       return {
-
+        drawer: false,
+        items: [
+          {title: 'Home', icon: 'dashboard'},
+          {title: 'About', icon: 'question_answer'}
+        ],
+        mini: false,
+        right: null
       }
     }
   }
